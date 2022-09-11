7c1e46a911595dc705f0f0b8e77591da731b78de
zidanegimiga <zidanegimiga@gmail.com>
2022-09-12 01:03:34 +0300

    feature

---
+++ src/pages/_document.jsx
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta property="og:type" content="website" key="og-type" />
+          <meta
+            property="og:url"
+            content="https://somasanaa.vercel.app/"
+            key="og-url"
+          />
+          <meta
+            property="og:title"
+            content="SomaSanaa"
+            key="og-title"
+          />
+          <link rel="icon" href="/logo.png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
