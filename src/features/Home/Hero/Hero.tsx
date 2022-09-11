850f9f00d31aa2ebb1b8371a6faf1d094a5487f1
zidanegimiga <zidanegimiga@gmail.com>
2022-09-12 00:46:40 +0300

    feat: create hero section

---
+++ src/features/Home/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Hero.module.scss';
+
+const Hero = () => {
+  return (
+    <div className={styles.heroContainer}>
+        Hero
+    </div>
+  )
+}
+
+export default Hero
