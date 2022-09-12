bfebd3edea11d3eb6ba706de9f658021a0e190a6
zidanegimiga <zidanegimiga@gmail.com>
2022-09-12 23:40:33 +0300

    style: make the class card responsive

--- src/features/Home/ClassCard/ClassCard.tsx
+++ src/features/Home/ClassCard/ClassCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link'
 import imgCard from '../../../../public/CardImage.png'
 import style from './ClassCard.module.scss';
 
@@ -10,9 +11,13 @@ const ClassCard = ({data}) => {
         <Image alt='Thumbnail' src={imgCard} width={566} height={353}/>
       </div>
       <div className={style.cardDescription}>
-        <h4>{data.title}</h4>
-        <h5>{data.author}</h5>
-        <p>{data.description}</p>
+      <Link 
+        href={`/?lesson=${data.id}`}
+        as={`/masterclass/${data.id}`}>
+        <h4>{data?.title}</h4>
+      </Link>
+        <h5>{data?.author}</h5>
+        <p>{data?.description}</p>
       </div>
     </div>
   )
