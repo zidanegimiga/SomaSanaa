2679f38a550c8570538d4a00f694f5bbde249b50
zidanegimiga <zidanegimiga@gmail.com>
2022-09-12 23:47:01 +0300

    feat: create navigation bar

---
+++ src/features/Home/Nav/Nav.tsx
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react';
+import style from './Nav.module.scss';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleClick = () =>{
+    if(!menuOpen){
+      setMenuOpen(true);
+    } else {
+      setMenuOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    const keyDownHandler = event => {
+      if (event.key === 'Escape') {
+        event.preventDefault();
+        setMenuOpen(false);
+      }
+    };
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className={style.NavContainer}>
+        <p>SomaSanaa</p>        
+        <div className={style.menuBtn} onClick={handleClick}>
+          <GiHamburgerMenu  className={style.hamburger} />
+        </div>        
+      </div>
+      {
+        menuOpen && (
+            <div className={style.MenuModal}>
+              <div className={style.NavItem}> Home </div>
+              <div className={style.NavItem}> Categories </div>
+              <div className={style.NavItem}> Contact Us </div>
+            </div>
+        )
+      } 
+    </>
+  );
+}
+
+export default Nav;
