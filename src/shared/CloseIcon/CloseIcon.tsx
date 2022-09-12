e5dbd4e86263e35d587889d420b1e62064443a21
zidanegimiga <zidanegimiga@gmail.com>
2022-09-12 23:45:50 +0300

    feat: create close button component

---
+++ src/shared/CloseIcon/CloseIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './CloseIcon.module.scss';
+
+const CloseIcon = () => {
+  return (
+    <span className={style.close}> X </span>
+  )
+}
+
+export default CloseIcon
