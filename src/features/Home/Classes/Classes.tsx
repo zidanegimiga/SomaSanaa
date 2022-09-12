3a4554c9db5eebe6f0e74a1ffd95716c7e093dd8
zidanegimiga <zidanegimiga@gmail.com>
2022-09-12 23:48:11 +0300

    feat: create classes component

--- src/features/Home/Classes/Classes.tsx
+++ src/features/Home/Classes/Classes.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
 import style from './Classes.module.scss';
 import ClassCard from '../ClassCard';
-import { cardData } from '../ClassCard/cardDescription';
 
-const Classes = () => {
+const Classes = ({data}) => {
   return (
     <div className={style.ClassesSection}>
       <h2> Recently Added</h2>
       <hr className={style.horizontalLine}/>
       <div className={style.ClassesContainer}>
         {
-          cardData.map((card, index) => (
-            <ClassCard data={card} key={index}/>
+          data?.map((lesson, index) => (
+            <div key={index}>
+              <ClassCard data={lesson}/>                    
+            </div>
           ))
-        }
+        } 
       </div>
     </div>
   )
