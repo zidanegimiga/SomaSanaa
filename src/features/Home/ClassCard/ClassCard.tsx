038b8b347ef2a3bead15589e95074a14429dd11e
zidanegimiga <zidanegimiga@gmail.com>
2022-09-19 13:56:04 +0300

    config: modify image asset relative path

--- src/features/Home/ClassCard/ClassCard.tsx
+++ src/features/Home/ClassCard/ClassCard.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link'
-import imgCard from '../../../../public/CardImage.png'
+import Link from 'next/link';
+import imgCard from '../../../../public/CardImage.png';
 import style from './ClassCard.module.scss';
 
 const ClassCard = ({data}) => {
