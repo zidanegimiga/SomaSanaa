552fbe7957b2acc9a4b275653212e5ef3a710998
zidanegimiga <zidanegimiga@gmail.com>
2022-09-27 08:26:20 +0300

    feat: add progress component library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
-import { FC } from 'react';
-import { AppProps } from 'next/app';
+import { FC } from "react";
+import { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 import "../styles/index.scss";
 
-const App:FC = ({ Component, pageProps }:AppProps) => {
-  return <Component {...pageProps} />
-}
-
+const App: FC = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Component {...pageProps} />
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+    </>
+  );
+};
 export default App;
