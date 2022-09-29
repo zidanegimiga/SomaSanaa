c054d56d0078abedc42e33741423e9547c20cd98
zidanegimiga <zidanegimiga@gmail.com>
2022-09-29 03:47:11 +0300

    feat: implement see more button

--- src/features/Home/Classes/Classes.tsx
+++ src/features/Home/Classes/Classes.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import Link from 'next/link';
-import style from './Classes.module.scss';
-import ClassCard from '../ClassCard';
+import React, { useState } from "react";
+import Link from "next/link";
+import style from "./Classes.module.scss";
+import ClassCard from "../ClassCard";
 
-const Classes = ({data}) => {
+const classPerView = 3;
+
+const Classes = ({ data }) => {
+  const [next, setNext] = useState(classPerView);
+
+  const seeMore = () => {
+    setNext(next + classPerView);
+  };
+
   return (
     <div className={style.ClassesSection}>
       <h2> Recently Added</h2>
-      <hr className={style.horizontalLine}/>
+      <hr className={style.horizontalLine} />
       <div className={style.ClassesContainer}>
-        {
-          data?.map((lesson, index) => (
-            <div key={index}>
-              <ClassCard data={lesson}/>                    
-            </div>
-          ))
-        } 
+        {data?.slice(0, next)?.map((lesson, index) => (
+          <div key={index}>
+            <ClassCard data={lesson} />
+          </div>
+        ))}
       </div>
+      {next < data?.length && (
+        <div className={style.seeMoreBtn}>
+          <input
+            type="button"
+            value="See more"
+            onClick={seeMore}
+            className={style.button}
+          />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Classes
+export default Classes;
