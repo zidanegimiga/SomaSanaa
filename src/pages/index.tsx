e216ef4a76a9786f74f38ed0c84eca3d1ebe640b
zidanegimiga <zidanegimiga@gmail.com>
2022-11-28 11:44:30 +0300

    style: add padding to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,9 @@ import Head from "next/head";
 import Hero from "features/Home/Hero";
 import Classes from "features/Home/Classes";
 import Nav from "shared/Nav";
-import {masterClassesData} from '../../data'
+import {masterClassesData} from '../../data';
 import Footer from "shared/Footer";
+import styles from '../styles/classes.module.scss';
 
 export default function Index({ payload, id }) { 
   return (
@@ -15,11 +16,13 @@ export default function Index({ payload, id }) {
       <Nav payload={payload}/>
       <Hero payload={payload}/>
       <Suspense>
-        <Classes data={payload} categoryTitle="Recently Added"/>
-        <Classes data={payload} categoryTitle="Organisation" category="organisation"/>
-        <Classes data={payload} categoryTitle="Video Production" category="video-production"/>
-        <Classes data={payload} categoryTitle="Music" category="music"/>
-        <Classes data={payload} categoryTitle="Digital Art" category="digital-art"/>
+        <div className={styles.classesWrapper}>
+          <Classes data={payload} categoryTitle="Recently Added"/>
+          <Classes data={payload} categoryTitle="Organisation" category="organisation"/>
+          <Classes data={payload} categoryTitle="Video Production" category="video-production"/>
+          <Classes data={payload} categoryTitle="Music" category="music"/>
+          <Classes data={payload} categoryTitle="Digital Art" category="digital-art"/>
+        </div>
       </Suspense>
       <Footer />
     </>
