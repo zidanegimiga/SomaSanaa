05517116d844b8ed0b374e596404b75e7793af77
zidanegimiga <zidanegimiga@gmail.com>
2022-12-09 13:27:40 +0300

    config: add analytics to platform

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
+import Script from "next/script";
 import "../styles/index.scss";
 
 const App: FC = ({ Component, pageProps }: AppProps) => {
+  const measurementID = "G-1JQZZ8NPTL"
   return (
     <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1JQZZ8NPTL" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1JQZZ8NPTL');
+        `}        
+      </Script>
       <Component {...pageProps} />
       <NextNProgress
         color="#29D"
